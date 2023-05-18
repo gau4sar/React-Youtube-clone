0095c59c651424891dde0dd7098674e0d3b2346d
GST <gaurav4sarma@gmail.com>
2023-05-18 16:02:14 +0530

    Router is updated for gh pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import { Box } from '@mui/material'
-import {NavBar,VideoDetail,Feed,ChannelDetail,SearchFeed,SearchBar} from './components'
+import React from "react";
+import { BrowserRouter, Route, Routes, useMatch } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  NavBar,
+  VideoDetail,
+  Feed,
+  ChannelDetail,
+  SearchFeed,
+  SearchBar,
+} from "./components";
 
-
-
-
-
-const App = () => (
-  <BrowserRouter>
-    <Box sx={{ backgroundColor: 'white' }}>
-      <NavBar/>
-      <Routes>
-        <Route exact path='/' element={<Feed />} />
-        <Route path='/video/:id' element={<VideoDetail />} />
-        <Route path='/channel/:id' element={<ChannelDetail />} />
-        <Route path='/search/:searchTerm' element={<SearchFeed />} />
-      </Routes>
-    </Box>
-  </BrowserRouter>
-);
+const App = () => {
+  const { url } = useMatch();
+  return (
+    <BrowserRouter basename="/React-Youtube-clone">
+      <Box sx={{ backgroundColor: "white" }}>
+        <NavBar />
+        <Routes>
+          <Route path={`${url}/`} element={<Feed />} />
+          <Route path={`${url}/video/:id`} element={<VideoDetail />} />
+          <Route path={`${url}/channel/:id`} element={<ChannelDetail />} />
+          <Route path={`${url}/search/:searchTerm`} element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
 export default App;
