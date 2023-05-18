afd66257bd640ce845f9dbdc7a90bbe945b2d901
GST <gaurav4sarma@gmail.com>
2023-05-18 16:31:44 +0530

    App and package are updated for deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import { Box } from '@mui/material'
-import {NavBar,VideoDetail,Feed,ChannelDetail,SearchFeed,SearchBar} from './components'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  NavBar,
+  VideoDetail,
+  Feed,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
-
-
-
-
-const App = () => (
-  <BrowserRouter>
-    <Box sx={{ backgroundColor: 'white' }}>
-      <NavBar/>
-      <Routes>
-        <Route exact path='/' element={<Feed />} />
-        <Route path='/video/:id' element={<VideoDetail />} />
-        <Route path='/channel/:id' element={<ChannelDetail />} />
-        <Route path='/search/:searchTerm' element={<SearchFeed />} />
-      </Routes>
-    </Box>
-  </BrowserRouter>
-);
+const App = () => {
+  return (
+    <BrowserRouter basename="/React-Youtube-clone">
+      <Box sx={{ backgroundColor: "white" }}>
+        <NavBar />
+        <h2>Test test =</h2>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
 export default App;
